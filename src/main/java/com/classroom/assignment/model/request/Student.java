8f0bb9fbaf70fdc8114c622020fe610fe1e91c81
Zanshin07 <139198337+Zanshin07@users.noreply.github.com>
2023-12-13 06:01:52 +0000

    課題４の所属学科が表示されるようにしました。
    Co-authored-by: アカティン01 <akatyann@users.noreply.github.com>
    Co-authored-by: 23cm0108 <23cm0108@users.noreply.github.com>

--- src/main/java/com/classroom/assignment/model/request/Student.java
+++ src/main/java/com/classroom/assignment/model/request/Student.java
@@ -4,9 +4,12 @@ public class Student {
   private String id;
   private String name;
 
-  public Student(String id, String name) {
+  private String className;
+
+  public Student(String id, String name, String className) {
     this.setId(id);
     this.setName(name);
+    this.setClassName(className);
   }
 
   public String getName() {
@@ -25,5 +28,12 @@ public class Student {
     this.id = id;
   }
 
+  public String getClassName() {
+    return className;
+  }
+
+  public void setClassName(String className) {
+    this.className = className;
+  }
 
 }
